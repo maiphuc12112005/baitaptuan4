2df26085cff5ff91e8be029ffd243dd487399e54
maiphuc12112005 <banm3024@gmail.com>
2025-06-04 15:39:08 +0700

    Bai-tap-ca-nhan

--- Baitaptuan3/navigation/app/src/main/java/com/example/navigation/data/models/OnboardingItem.kt
+++ Baitaptuan3/navigation/app/src/main/java/com/example/navigation/data/models/OnboardingItem.kt
@@ -2,11 +2,9 @@ package com.example.navigation.data.models
 
 import androidx.annotation.DrawableRes
 
-/**
- * Data class biểu diễn một mục (trang) trong quá trình onboarding.
- */
+
 data class OnboardingItem(
-    @DrawableRes val imageResId: Int, // ID tài nguyên hình ảnh
+    @DrawableRes val imageResId: Int,
     val title: String,
     val description: String
 )
